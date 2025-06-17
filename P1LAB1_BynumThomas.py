0b3600c662ab57c238f118af67bac498b4e8c928
bynumt0602 <bynumt0602@student.faytechcc.edu>
2025-06-16 22:10:53 -0400

    Add files via upload
    First Submission.

---
+++ P1LAB1_BynumThomas.py
@@ -0,0 +1,15 @@
+ # Thomas Bynum
+ # 06/16/2025
+ # P1LAB1
+ # This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.
+
+
+print("Enter your first name: ")
+
+first_name = input()
+
+print("Enter your last name: ")
+
+last_name = input()
+
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
